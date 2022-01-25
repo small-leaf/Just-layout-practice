acd7a9ae9dfe98b60849327f426b2b0f9979e2e9
葉嘉文 <hh741456963@gmail.com>
2022-01-25 10:38:50 +0800

    feat: add customerService.

---
+++ customerService/src/js/customerService/index.js
@@ -0,0 +1,13 @@
+// import zendesk from "./zendesk"
+// import tawk from "./tawk"
+// import jivo from "./jivo"
+import livechat from "./livechat"
+import respondio from './respondio';
+
+export default {
+//   zendesk,
+//   tawk,
+//   jivo,
+  livechat,
+  respondio,
+}
